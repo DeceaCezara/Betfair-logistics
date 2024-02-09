07b0c5cd359d0c5d9849955be8a624759aa153a5
Decea Cezara <deceacezara2003@gmail.com>
2024-02-09 20:15:08 +0200

    Create tables and populate from csv.

--- src/main/java/com/betfair/logistics/dao/model/Destination.java
+++ src/main/java/com/betfair/logistics/dao/model/Destination.java
@@ -1,4 +1,31 @@
 package com.betfair.logistics.dao.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Entity
+@Table(name = "destinations")
+@EqualsAndHashCode
 public class Destination {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //de obicei se foloseste sequence(+ se poate folosi acelasi sequence pt mai multe tabele) sau auto
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer distance;
+
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "destination")
+//    List<Order> orders;
+
+
+
+
+
+
 }
---
+++ src/main/java/com/betfair/logistics/dao/model/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.betfair.logistics.dao.model;
+
+public enum OrderStatus {
+    NEW,DELIVERING,SHIPPING,DELIVERED,CANCELLED;
+}
